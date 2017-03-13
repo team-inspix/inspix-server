6e932ea928477cea38d615fcc6f611af1a27f8fe
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-03-13 17:15:17 +0900

    implement /register endpoint

--- index.py
+++ index.py
@@ -1,11 +1,17 @@
-from flask import Flask
+# coding: utf-8
+from __future__ import unicode_literals
+
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import hashlib
 from datetime import datetime
+import sqlite3
+
 
 # global variables
 app = Flask('inspix')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['JSON_AS_ASCII'] = False 
 db = SQLAlchemy(app)
 
 # constants
@@ -34,6 +40,37 @@ class User(db.Model):
         
     def check_password(self, password):
         return self.password == self.generate_password(password)
+    
+def make_error_json(errorstr):
+    json = {
+        "data": {},
+        "error": [errorstr]
+    }
+    return jsonify(json)
+
+def make_data_json(data):
+    json = {
+        "data": data,
+        "error": []
+    }
+    return jsonify(json)
+    
+@app.route('/register', methods=['POST'])
+def register():
+    response = dict()
+    try:
+        jsondata = request.json
+        user = User(jsondata['name'],  jsondata['password'])
+        db.session.add(user)
+        db.session.commit()
+        
+        response = {"id": user.id}
+        
+    except Exception as e:
+        return make_error_json("ユーザ名は既に使用されています")
+        #return make_error_json(str(type(e)))
+        
+    return make_data_json(response)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
